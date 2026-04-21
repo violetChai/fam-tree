6c0547eb085e647758db69ddb380db2f558efd32
Lynn <contactlynnp@gmail.com>
2026-04-21 16:12:51 +1000

    backend setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./config/db");
 require("dotenv").config();
+
+connectDB();
 
 const app = express();
 
